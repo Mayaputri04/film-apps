9058e307af1ff24bdf56d2845869cd61e4aac39e
Mayaputri04 <166621544+Mayaputri04@users.noreply.github.com>
2025-05-22 15:01:34 +0700

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
+
 
 module.exports = merge(common, {
   mode: 'production',
@@ -34,9 +34,6 @@ module.exports = merge(common, {
     new InjectManifest({
       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: 'sw.bundle.js',
-    }),
-    new GenerateSW({
-      swDest: 'sw.workbox.bundle.js',
     }),
   ]
 });
